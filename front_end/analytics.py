31a280f8242f3299a08ef3fbb972ca5df4e71fea
Celsosoarescesar <62956614+Celsosoarescesar@users.noreply.github.com>
2023-10-23 16:35:55 -0300

    Update analytics.py
    modelo final

--- front_end/analytics.py
+++ front_end/analytics.py
@@ -9,7 +9,10 @@ import front_end.constantes as const
 def calc_display_basic_metrics(data, location=st):
 	
 	const.spacer('title')
-
+        location.subheader(f'Valor do Empréstimo: R$ {data.LoanAmount}')
+	location.subheader(f'Prazo do Empréstimo: {data.Loan_Amount_Term} Meses')
+	location.subheader(f'Valor da Parcela : R$ {round(npf.pmt(0.1275/12,data.Loan_Amount_Term,-data.LoanAmount,0),2)}')
+	location.subheader(f'Taxa de juros: {12.75} Ao Ano')
 
 # Mostra os resultados
 def display_all_results(data, location=st):
